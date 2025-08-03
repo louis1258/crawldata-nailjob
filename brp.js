f2cde9dc7ead0c6eb6e8468212c750c5672fc22c
louis <nghia@gmail.com>
2025-08-04 00:44:51 +0900

    update

--- brp.js
+++ brp.js
@@ -300,7 +300,7 @@ async function crawlSingleUrl(browser, href, stateName) {
                 let addressText = '';
                 try {
                     addressText = await page.$eval(
-                        'div[id^="id"] > div.ellipsis + div',
+                        '#ad_vi',
                         el => el.innerText.trim()
                     );
                 } catch (error) {
@@ -461,7 +461,7 @@ connect({
         '--disable-gpu',
         '--no-sandbox',
     ],
-    // proxy: proxies[0]
+    proxy: proxies[0]
 })
     .then(async response => {
         let { browser, page } = response;
